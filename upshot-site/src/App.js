37e3536ac8caebbff50e772010d7dfe3c087bd49
Bruce <superunknown1103@gmail.com>
2020-01-05 12:44:46 -0600

    header, columns

--- upshot-site/src/App.js
+++ upshot-site/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row'>
+        <div id="logo">
+
+        </div>
+       <div className="column" id="pageHeader">
+         THIS IS A HEADLINE.
+       </div>
+      </div>
+      <div className='row 2'>
+        <div className='column'>
+          <div className='menu'>
+            <ul className="menuOptions">
+
+            </ul>
+      </div>
+        </div>
+        <div className='double-column'>
+          <div className='inner-column'>
+            Some Text in Column Two
+      </div>
+        </div>
+      </div>
     </div>
   );
 }
